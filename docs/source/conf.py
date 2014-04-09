748e4a152117151d0612cbbab43cd87d3f1b76e0
Michael Joyce <joyce@apache.org>
2014-04-08 19:40:34 -0700

    Trying to fix dependency problems

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -269,3 +269,28 @@ intersphinx_mapping = {'http://docs.python.org/': None}
 # "both"  - The class' and __init__ method's docstring
 # "init"  - Only __init__'s docstring
 autoclass_content = "both"
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['netCDF4']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
